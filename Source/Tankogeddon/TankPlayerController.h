21783876793ac47024ddcf08259ea2ead2c7766c
Alexander Rybalka <polterageist@yandex.ru>
2021-07-04 00:21:07 +0300

    Initial commit

---
+++ Source/Tankogeddon/TankPlayerController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKOGEDDON_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void MoveForward(float AxisValue);
+
+private:
+	UPROPERTY()
+	class ATankPawn* TankPawn;
+};
